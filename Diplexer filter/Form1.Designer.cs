5597807a7f203dcab079f60a87149ecbcb59f5b6
la3pna <la3pna@gmail.com>
2013-09-10 16:08:23 +0200

    project added

---
+++ Diplexer filter/Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace Diplexer_filter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.textBox9 = new System.Windows.Forms.TextBox();
+            this.textBox10 = new System.Windows.Forms.TextBox();
+            this.textBox11 = new System.Windows.Forms.TextBox();
+            this.textBox12 = new System.Windows.Forms.TextBox();
+            this.textBox13 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(65, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Frequency (MHz)";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(303, 19);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(14, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "K";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(93, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(282, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(55, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "1,005";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(538, 35);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(78, 19);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calculate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(422, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(15, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "R";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(404, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(55, 20);
+            this.textBox3.TabIndex = 6;
+            this.textBox3.Text = "50";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Diplexer_filter.Properties.Resources.diplexer8;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 81);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(632, 369);
+            this.pictureBox1.TabIndex = 7;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(158, 106);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(69, 20);
+            this.textBox4.TabIndex = 8;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(190, 154);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.ReadOnly = true;
+            this.textBox5.Size = new System.Drawing.Size(69, 20);
+            this.textBox5.TabIndex = 9;
+            this.textBox5.TextChanged += new System.EventHandler(this.textBox5_TextChanged);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(338, 106);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.ReadOnly = true;
+            this.textBox6.Size = new System.Drawing.Size(69, 20);
+            this.textBox6.TabIndex = 10;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(377, 154);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.ReadOnly = true;
+            this.textBox7.Size = new System.Drawing.Size(69, 20);
+            this.textBox7.TabIndex = 11;
+            this.textBox7.TextChanged += new System.EventHandler(this.textBox7_TextChanged);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(524, 106);
+            this.textBox8.Name = "textBox8";
+            this.textBox8.ReadOnly = true;
+            this.textBox8.Size = new System.Drawing.Size(69, 20);
+            this.textBox8.TabIndex = 12;
+            // 
+            // textBox9
+            // 
+            this.textBox9.Location = new System.Drawing.Point(24, 221);
+            this.textBox9.Name = "textBox9";
+            this.textBox9.ReadOnly = true;
+            this.textBox9.Size = new System.Drawing.Size(69, 20);
+            this.textBox9.TabIndex = 13;
+            // 
+            // textBox10
+            // 
+            this.textBox10.Location = new System.Drawing.Point(123, 341);
+            this.textBox10.Name = "textBox10";
+            this.textBox10.ReadOnly = true;
+            this.textBox10.Size = new System.Drawing.Size(69, 20);
+            this.textBox10.TabIndex = 14;
+            // 
+            // textBox11
+            // 
+            this.textBox11.Location = new System.Drawing.Point(174, 237);
+            this.textBox11.Name = "textBox11";
+            this.textBox11.ReadOnly = true;
+            this.textBox11.Size = new System.Drawing.Size(69, 20);
+            this.textBox11.TabIndex = 15;
+            this.textBox11.TextChanged += new System.EventHandler(this.textBox11_TextChanged);
+            // 
+            // textBox12
+            // 
+            this.textBox12.Location = new System.Drawing.Point(404, 237);
+            this.textBox12.Name = "textBox12";
+            this.textBox12.ReadOnly = true;
+            this.textBox12.Size = new System.Drawing.Size(69, 20);
+            this.textBox12.TabIndex = 16;
+            this.textBox12.TextChanged += new System.EventHandler(this.textBox12_TextChanged);
+            // 
+            // textBox13
+            // 
+            this.textBox13.Location = new System.Drawing.Point(306, 341);
+            this.textBox13.Name = "textBox13";
+            this.textBox13.ReadOnly = true;
+            this.textBox13.Size = new System.Drawing.Size(69, 20);
+            this.textBox13.TabIndex = 17;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(537, 433);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 13);
+            this.label4.TabIndex = 18;
+            this.label4.Text = "LA3PNA 2013";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(43, 428);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(160, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Inductors in µH, capacitors in pF";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(541, 320);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(74, 40);
+            this.button2.TabIndex = 21;
+            this.button2.Text = "Save SPICE";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(15, 12);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(0, 13);
+            this.label5.TabIndex = 22;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(656, 458);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox13);
+            this.Controls.Add(this.textBox12);
+            this.Controls.Add(this.textBox11);
+            this.Controls.Add(this.textBox10);
+            this.Controls.Add(this.textBox9);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.TextBox textBox9;
+        private System.Windows.Forms.TextBox textBox10;
+        private System.Windows.Forms.TextBox textBox11;
+        private System.Windows.Forms.TextBox textBox12;
+        private System.Windows.Forms.TextBox textBox13;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ Diplexer filter/Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Diplexer_filter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox12_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float fc = float.Parse(textBox1.Text);
+            float k = float.Parse(textBox2.Text);
+            float r = float.Parse(textBox3.Text);
+            double w = 2 * Math.PI * (fc*Math.Pow(10, 6));
+            double t4 = ((k * 1.5613 * r) / (w)) * Math.Pow(10, 6);
+            double t5 = ((k*1.8069)/(r*w))*Math.Pow(10, 12);
+            double t6 = ((k * 1.7659 * r) / (w)) * Math.Pow(10, 6);
+            double t7 = ((k * 1.4173) / (r * w)) * Math.Pow(10, 12);
+            double t8 = ((k * 0.6507 * r) / (w)) * Math.Pow(10, 6);
+
+            double t9 = ((0.6405) / (r * k * w)) * Math.Pow(10, 12);
+            double t10 = (( 0.5534 * r) / (k * w)) * Math.Pow(10, 6);
+            double t11 = ((0.5663) / (r * k * w)) * Math.Pow(10, 12);
+            double t13 = ((0.7056 * r) / (k * w)) * Math.Pow(10, 6);
+            double t12 = ((1.5368) / (r * k * w)) * Math.Pow(10, 12);
+            
+
+
+          
+
+            //Math.Pow(10, 12)
+            // String.Format("{0:0.00}", (ser))
+            textBox4.Text = Convert.ToString(String.Format("{0:0.000}", (t4)));
+            textBox5.Text = Convert.ToString(String.Format("{0:0.00}", (t5)));
+            textBox6.Text = Convert.ToString(String.Format("{0:0.000}", (t6)));
+            textBox7.Text = Convert.ToString(String.Format("{0:0.00}", (t7)));
+            textBox8.Text = Convert.ToString(String.Format("{0:0.000}", (t8)));
+            textBox9.Text = Convert.ToString(String.Format("{0:0.00}", (t9)));
+            textBox10.Text = Convert.ToString(String.Format("{0:0.000}", (t10)));
+            textBox11.Text = Convert.ToString(String.Format("{0:0.00}", (t11)));
+            textBox12.Text = Convert.ToString(String.Format("{0:0.00}", (t12)));
+            textBox13.Text = Convert.ToString(String.Format("{0:0.000}", (t13)));
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            label4.Text = "la3pna@gmail.com";
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox11_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+           Stream myStream ; 
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog(); 
+
+            saveFileDialog1.Filter = "SPICE FILE (*.asc)|*.asc|All files (*.*)|*.*" ; 
+            saveFileDialog1.FilterIndex = 1 ; 
+            saveFileDialog1.RestoreDirectory = true ; 
+
+            if(saveFileDialog1.ShowDialog() == DialogResult.OK) 
+            { 
+                  if((myStream = saveFileDialog1.OpenFile()) != null) 
+                  { 
+                        StreamWriter wText =new StreamWriter(myStream);
+
+                        wText.WriteLine("Version 4");
+                        wText.WriteLine("SHEET 1 880 680");
+                        wText.WriteLine("WIRE -48 -16 -64 -16");
+                        wText.WriteLine("WIRE 64 -16 32 -16");
+                        wText.WriteLine("WIRE 112 -16 64 -16");
+                        wText.WriteLine("WIRE 112 0 112 -16");
+                        wText.WriteLine("WIRE 224 0 192 0");
+                        wText.WriteLine("WIRE 256 0 224 0");
+                        wText.WriteLine("WIRE 384 0 336 0");
+                        wText.WriteLine("WIRE 432 0 384 0");
+                        wText.WriteLine("WIRE 544 0 512 0");
+                        wText.WriteLine("WIRE 224 16 224 0");
+                        wText.WriteLine("WIRE 384 16 384 0");
+                        wText.WriteLine("WIRE 544 16 544 0");
+                        wText.WriteLine("WIRE -64 32 -64 -16");
+                        wText.WriteLine("WIRE 64 32 64 -16");
+                        wText.WriteLine("WIRE 224 96 224 80");
+                        wText.WriteLine("WIRE 384 96 384 80");
+                        wText.WriteLine("WIRE 544 96 544 80");
+                        wText.WriteLine("WIRE -64 144 -64 112");
+                        wText.WriteLine("WIRE 64 208 64 96");
+                        wText.WriteLine("WIRE 128 208 64 208");
+                        wText.WriteLine("WIRE 192 208 128 208");
+                        wText.WriteLine("WIRE 320 208 256 208");
+                        wText.WriteLine("WIRE 368 208 320 208");
+                        wText.WriteLine("WIRE 512 208 432 208");
+                        wText.WriteLine("WIRE 512 224 512 208");
+                        wText.WriteLine("WIRE 128 304 128 288");
+                        wText.WriteLine("WIRE 320 304 320 288");
+                        wText.WriteLine("WIRE 512 320 512 304");
+                        wText.WriteLine("FLAG 128 304 0");
+                        wText.WriteLine("FLAG 320 304 0");
+                        wText.WriteLine("FLAG 512 320 0");
+                        wText.WriteLine("FLAG 384 96 0");
+                        wText.WriteLine("FLAG 224 96 0");
+                        wText.WriteLine("FLAG -64 144 0");
+                        wText.WriteLine("FLAG 544 96 0"); //END of all tedious wiring
+                        wText.WriteLine("SYMBOL res 496 208 R0");
+                        wText.WriteLine("SYMATTR InstName R1");
+                        wText.Write("SYMATTR Value ");
+                        wText.WriteLine(textBox3.Text);
+                        wText.WriteLine("SYMBOL cap 48 32 R0");
+                        wText.WriteLine("SYMATTR InstName C1");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox9.Text);
+                        wText.WriteLine("p");
+                        wText.WriteLine("SYMBOL cap 208 16 R0");
+                        wText.WriteLine("SYMATTR InstName C2");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox5.Text);
+                        wText.WriteLine("p");
+                        wText.WriteLine("SYMBOL cap 368 16 R0");
+                        wText.WriteLine("SYMATTR InstName C3");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox7.Text);
+                        wText.WriteLine("p");
+                        wText.WriteLine("SYMBOL cap 256 192 R90");
+                        wText.WriteLine("WINDOW 0 0 32 VBottom 2");
+                        wText.WriteLine("WINDOW 3 32 32 VTop 2");
+                        wText.WriteLine("SYMATTR InstName C4");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox11.Text);
+                        wText.WriteLine("p");
+                        wText.WriteLine("SYMBOL cap 432 192 R90");
+                        wText.WriteLine("WINDOW 0 0 32 VBottom 2");
+                        wText.WriteLine("WINDOW 3 32 32 VTop 2");
+                        wText.WriteLine("SYMATTR InstName C5");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox12.Text);
+                        wText.WriteLine("p");
+                        wText.WriteLine("SYMBOL ind 208 16 M270");
+                        wText.WriteLine("WINDOW 0 32 56 VTop 2");
+                        wText.WriteLine("WINDOW 3 5 56 VBottom 2");
+                        wText.WriteLine("SYMATTR InstName L1");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox4.Text);
+                        wText.WriteLine("µ");
+                        wText.WriteLine("SYMBOL ind 352 16 M270");
+                        wText.WriteLine("WINDOW 0 32 56 VTop 2");
+                        wText.WriteLine("WINDOW 3 5 56 VBottom 2");
+                        wText.WriteLine("SYMATTR InstName L2");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox6.Text);
+                        wText.WriteLine("µ");
+                        wText.WriteLine("SYMBOL ind 528 16 M270");
+                        wText.WriteLine("WINDOW 0 32 56 VTop 2");
+                        wText.WriteLine("WINDOW 3 5 56 VBottom 2");
+                        wText.WriteLine("SYMATTR InstName L3");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox8.Text);
+                        wText.WriteLine("µ");
+                        wText.WriteLine("SYMBOL ind 144 192 M0");
+                        wText.WriteLine("SYMATTR InstName L4");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox10.Text);
+                        wText.WriteLine("µ");
+                        wText.WriteLine("SYMBOL ind 336 192 M0");
+                        wText.WriteLine("SYMATTR InstName L5");
+                        wText.Write("SYMATTR Value ");
+                        wText.Write(textBox13.Text);
+                        wText.WriteLine("µ");
+                        wText.WriteLine("SYMBOL voltage -64 16 R0");
+                        wText.WriteLine("WINDOW 123 0 0 Left 2");
+                        wText.WriteLine("WINDOW 39 0 0 Left 2");
+                        wText.WriteLine("SYMATTR InstName V1");
+                        wText.WriteLine("SYMATTR Value AC 2");
+                        wText.WriteLine("SYMBOL res 48 0 M270");
+                        wText.WriteLine("WINDOW 0 32 56 VTop 2");
+                        wText.WriteLine("WINDOW 3 0 56 VBottom 2");
+                        wText.WriteLine("SYMATTR InstName R2");
+                        wText.Write("SYMATTR Value ");
+                        wText.WriteLine(textBox3.Text);
+                        wText.WriteLine("SYMBOL res 528 -16 R0");
+                        wText.WriteLine("SYMATTR InstName R3");
+                        wText.Write("SYMATTR Value ");
+                        wText.WriteLine(textBox3.Text);
+                        wText.WriteLine("TEXT 200 344 Left 2 !.ac lin 500 1meg 30meg");
+                       
+                        wText.Flush();
+                        wText.Close();
+                        label5.Text = "SAVED";
+                  } 
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
